17a621ad05275ec52aa65b149b47ec932e1752d3
Colby Endres <70925616+colbyendres@users.noreply.github.com>
2025-06-17 10:02:40 -0500

    Use functions in `MLIExec` (#13)
    * Clean up helper functions, formatting for messages
    
    * Permit multiple return values
    
    * Use print for legacy APFloat
    
    * Fix breakage in parsing due to whitespace

--- lib/Interpreter/InterpreterOpInterface.cpp
+++ lib/Interpreter/InterpreterOpInterface.cpp
@@ -6,8 +6,7 @@
 /// Include the definitions of the interpreter op interface.
 #include "mlir/Interpreter/InterpreterOpInterface.cpp.inc"
 
-using namespace mlir;
-
+namespace mlir {
 EvalError::EvalError(llvm::StringRef message, bool shouldPrintStackTrace)
     : message(message.str()) {
   if (shouldPrintStackTrace) {
@@ -15,3 +14,52 @@ EvalError::EvalError(llvm::StringRef message, bool shouldPrintStackTrace)
     llvm::sys::PrintStackTrace(stream);
   }
 }
+
+llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const EvalValue& val) {
+    // Special case i1 = bool
+    auto valType = val.getType();
+    if (valType.isInteger(sizeof(bool))) {
+        auto data = val.getData<bool>();
+        if (!data.empty()) {
+            os << data[0];
+        }
+    }
+    // Handle integer types
+    else if (valType.isIntOrIndex()) {
+        auto data = val.getData<APInt>();
+        if (!data.empty()) {
+            os << data[0];
+        }
+    }
+    // Handle float types
+    else if (mlir::isa<mlir::FloatType>(valType)) {
+        auto data = val.getData<APFloat>();
+        if (!data.empty()) {
+            data[0].print(os);
+        }
+    }
+    // Handle pointer types
+    else if (mlir::isa<mlir::LLVM::LLVMPointerType>(valType)) {
+        auto data = val.getData<uint64_t>();
+        if (!data.empty()) {
+            os << "0x";
+            os.write_hex(data[0]);
+        }
+    }
+    // Default fallback - show raw bytes in hex
+    else {
+        const char* rawData = val.getRawData();
+        size_t dataSize = val.getRawDataSizeInBytes();
+
+        os << "0x";
+        for (size_t i = 0; i < dataSize; i++) {
+            // Format each byte as hex
+            char buffer[3];
+            snprintf(buffer, sizeof(buffer), "%02x", (unsigned char)rawData[i]);
+            os << buffer;
+        }
+    }
+    return os;
+}
+}
+
--- test/llvm_ir/trunc/test_trunc.mlir
+++ test/llvm_ir/trunc/test_trunc.mlir
@@ -1,4 +1,4 @@
-func.func @main(%arg0: i64) -> i32 {
-  %res = llvm.trunc %arg0 : i64 to i32
-  llvm.return %res : i32
+func.func @main(%arg0: i32) -> i8 {
+  %res = llvm.trunc %arg0 : i32 to i8
+  llvm.return %res : i8
 }
